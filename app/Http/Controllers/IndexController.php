963b665e2ccb16218927fafc422c7f59878d9f88
Gliufh <760404838@qq.com>
2021-06-03 13:57:23 +0800

    上传

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,16 @@ use Illuminate\Support\Facades\Storage;
 class IndexController extends Controller
 {
     public function upload(Request $request){
-        $url = Storage::putFile('public/github/' . date("Ymd"), $request->file('file'));
-        return $this->response(env('APP_URL').'/storage/'.str_replace('public/', '', $url));
+        $path = Storage::putFile('public/github/' . date("Ymd"), $request->file('file'));
+        $url = env('APP_URL').'/storage/'.str_replace('public/', '', $path);
+        if ($request->input('source') == "mdEditor"){
+            return response([
+                'success' => 1,
+                'message' => "上传成功",
+                'url' => $url
+            ]);
+        }else {
+            return $this->response($url);
+        }
     }
 }
