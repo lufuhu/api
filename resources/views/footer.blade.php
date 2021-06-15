200d848a0294acf9c7ac950db8242b54e3fc1559
Gliufh <760404838@qq.com>
2021-06-15 14:19:38 +0800

    1

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,5 +5,39 @@
     }
 </style>
 <div class="footer">
-
+    <div class="w-main mx-auto max-w-screen-xl flex flex-col justify-center items-center">
+        <div class="flex mt-16 mb-12">
+            <a class="footer-icon-1" href="https://github.com/lufuhu" target="_blank"><img src="https://www.lufuhu.com/static/icon/github.png"/></a>
+            <a class="footer-icon-1" href="https://gitee.com/lufuhu" target="_blank"><img src="https://www.lufuhu.com/static/icon/gitee.png"/></a>
+            <el-popover
+                placement="top"
+                width="200"
+                trigger="hover">
+                <div slot="reference">
+                    <div class="footer-icon-1"><img src="https://www.lufuhu.com/static/icon/wexin.png"/></div>
+                </div>
+                <img src="https://www.lufuhu.com/static/wechat.jpg"/>
+            </el-popover>
+            <el-popover
+                placement="top"
+                width="200"
+                trigger="hover">
+                <div slot="reference">
+                    <div class="footer-icon-1"><img src="https://www.lufuhu.com/static/icon/qq.png"/></div>
+                </div>
+                <img src="https://www.lufuhu.com/static/qq.png"/>
+            </el-popover>
+            <el-tooltip class="item" effect="dark" content="master@lufuhu.com" placement="top">
+                <div class="footer-icon-1"><img src="https://www.lufuhu.com/static/icon/mail.png"/></div>
+            </el-tooltip>
+        </div>
+        <div class="text-gray-300 text-sm flex justify-center items-center">
+            <el-link href="https://beian.miit.gov.cn" target="_blank">鄂ICP备2021009343号-1</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <div class="flex items-center">
+                <img src="https://www.lufuhu.com/static/gongan.png" class="mr-1"/>
+                <el-link href="http://www.beian.gov.cn" target="_blank">鄂公网安备号</el-link>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,83 +8,24 @@
     <meta name="keywords" content="编程问号,lufuhu">
     <meta name="author" content="编程问号">
     <link rel="icon" href="/favicon.ico">
-    <title>编程问号 | 登录</title>
+    <title>LUFUHU | 编程问号</title>
     <style>
         *, html {
             margin: 0;
             padding: 0;
         }
-
         a {
             color: inherit;
             text-decoration: inherit;
         }
-
-        .header {
-            height: 60px;
-            z-index: 50;
-            position: fixed;
-            width: 100%;
-            background: #ffffff;
-        }
-
-        .border-b {
-            border-bottom: 1px solid #e5e7eb;
-        }
-
         .w-main {
             width: 1024px;
             margin: 0 auto;
         }
-
-        .header .logo img {
-            width: 56px;
-            height: 56px;
-            border-radius: 50%;
-        }
-
-        .header .logo p {
-            font-weight: 700;
-        }
-
-        .flex {
-            display: flex;
-        }
-
-        .items-center {
-            align-items: center;
-        }
-
-        .justify-center {
-            justify-content: center;
-        }
-
-        .justify-between {
-            justify-content: space-between;
-        }
-
         .content {
             min-height: calc(100vh - 330px);
             padding-top: 80px;
             padding-bottom: 50px;
-        }
-
-        .avatar {
-            height: 30px;
-            width: 30px;
-            border-radius: 50%;
-        }
-
-        .sub-title {
-            margin-top: 15px;
-            margin-bottom: 30px;
-            padding-bottom: 15px;
-            font-size: 14px;
-            color: #52585f;
-        }
-
-        .text-center {
-            text-align: center;
         }
     </style>
     <style>
@@ -107,17 +48,9 @@
     </style>
 </head>
 <body>
-<div class="header border-b">
-    <div class="w-main flex items-center justify-between">
-        <a href="/" class="logo flex items-center">
-            <img src="https://www.lufuhu.com/favicon.ico">
-            <p>编程问号</p>
-        </a>
-        <a href="/user" class="flex items-center">
-            <img class="avatar" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png">
-        </a>
-    </div>
-</div>
+
+@include("header")
+
 <div class="content">
     <div class="w-main">
         @if ($ok)
